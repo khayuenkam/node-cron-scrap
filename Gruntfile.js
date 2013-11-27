69c2d6314e505babea107bf01a4f5984d2e0c658
khayuen <khayuen@yahoo.com>
2013-11-27 14:25:57 +0000

    update gruntfile and package.json and add .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,26 @@
+'use strict';
+
 module.exports = function(grunt) {
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
+      files: ['Gruntfile.js', 'lib/**/*.js', 'test/*.js', 'index.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    mochaTest: {
+      src: ['test/*.js'],
+      options: {
+        globals: ['chai'],
+        timeout: 6000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'spec'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
 };
