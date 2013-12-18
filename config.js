a795e02ff7f40daa51a58686c0aef7c28685fed8
khayuen <khayuen@yahoo.com>
2013-12-18 15:39:32 +0000

    update config.js and package.json

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.hypebeast = {
-  url: 'http://hypebeast.com/' 
+  url: 'http://hypebeast.com/',
+  name: 'hypebeast'
 };
 
 exports.streething = {
@@ -11,15 +12,7 @@ exports.highsnobiety = {
 }
 
 exports.elasticsearch = {
-  indexName: 'es-magazines',
-  documentName: 'magazines',
-  server: {
-    host: 'api.searchbox.io',
-    port: 80,
-    secure: false,
-    auth: {
-      username: 'site',
-      apiKey: process.env.SEARCHLY_API_KEY
-    }
-  }
+  index: 'es-magazines',
+  type: 'magazines',
+  url: process.env.SEARCHLY_API_URL
 }
